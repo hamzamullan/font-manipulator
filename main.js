82af50f921f7ab5f5a6c73b06ec372895adf0570
hamzamullan <80555783+hamzamullan@users.noreply.github.com>
2022-09-06 18:50:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(600,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log(' PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+function draw(){
+    background('#AAAAAA');
+}
